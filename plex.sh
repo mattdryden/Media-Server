aed75130c28e101e8169e34ed709f62e15d8dd65
Matt Dryden <48354684+mattdryden@users.noreply.github.com>
2019-03-16 14:28:23 +0000

    Updated plex ports

--- plex.sh
+++ plex.sh
@@ -2,4 +2,4 @@
 
 echo "Create plex"
 #docker stop plex && docker rm plex
-docker run -d --restart unless-stopped --name=plex --net=host -e VERSION=latest -e PUID=0 -e PGID=0 -v /tmp:/transcode -v /mnt/data/config/plex:/config -v /mnt/data/media:/data/media linuxserver/plex
+docker run -d --restart unless-stopped --name=plex -p 32400:32400/tcp -p 32400:32400/udp -e VERSION=latest -e PUID=0 -e PGID=0 -v /tmp:/transcode -v /mnt/data/plex:/config -v /mnt/data/media:/data/media -h avalon linuxserver/plex
